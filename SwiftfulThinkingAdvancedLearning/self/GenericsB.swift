329afd88fe6a58305c984d56e816e7076374aac2
Baris Karalar <karalarbaris@gmail.com>
2024-03-13 15:21:55 +0100

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearning/self/GenericsB.swift
@@ -0,0 +1,18 @@
+//
+//  GenericsB.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Baris Karalar on 13.03.24.
+//
+
+import SwiftUI
+
+struct GenericsB: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenericsB()
+}
